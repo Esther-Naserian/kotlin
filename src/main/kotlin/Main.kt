57a92438600102e755ae0c076c962d024a73cac7
Esther-Naserian <naseriansamuria@gmail.com>
2024-02-08 08:21:47 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var fullName = ("EstherNaserian")
+    println("fullName")
+    var  studentAge = ("19years")
+    println("studentAge")
+    var studentWeight = ("50.4")
+    println("studentWeight")
+    var phoneNumber = ("0792804646")
+    println("phoneNumber")
+    var citizen = ("kenyan")
+    println("citizen")
+
+}
+
